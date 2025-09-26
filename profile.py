2b2a457e289815ccd0a2754c66e86c714098a75a
Idhibhat Pankam <marginpankam@gmail.com>
2025-09-26 15:42:20 +0700

    new disk image

--- profile.py
+++ profile.py
@@ -5,7 +5,7 @@ request = portal.context.makeRequestRSpec()
 # Here we assign constants that will be used later to describe our machines.
 DEFAULT_NODE_HARDWARE_TYPE = "c6525-25g"
 DEFAULT_DISK_IMAGE = (
-    "urn:publicid:IDN+utah.cloudlab.us+image+faasnetworkstack-PG0:base-setup"
+    "urn:publicid:IDN+utah.cloudlab.us+image+faasnetworkstack-PG0:baseNode"
 )
 DEFAULT_LAN_SOCKET = "eth1"
 lan = request.LAN()
