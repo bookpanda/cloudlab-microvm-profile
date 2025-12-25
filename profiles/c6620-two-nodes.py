f241c4bd8357bde3a06b4da9bc9007557c163a94
Idhibhat Pankam <marginpankam@gmail.com>
2025-12-25 14:55:55 +0700

    2

--- profiles/c6620-two-nodes.py
+++ profiles/c6620-two-nodes.py
@@ -2,25 +2,27 @@ import geni.portal as portal
 import geni.rspec.pg as pg
 
 pc = portal.Context()
-request = portal.context.makeRequestRSpec()
-# Here we assign constants that will be used later to describe our machines.
-DEFAULT_NODE_HARDWARE_TYPE = "c6620"
-DEFAULT_DISK_IMAGE = (
-    "urn:publicid:IDN+utah.cloudlab.us+image+faasnetworkstack-PG0:baseNodeV4"
-)
-DEFAULT_LAN_SOCKET = "eth1"
-lan = request.LAN()
-# Here we say that we want to use 4 machines for our experiment. We give unique names to each of these machines. Then we assign them to variables so we can give more information later on the hardware, software, and network connections of each machine.
-node_1 = request.RawPC("node_1")
-node_2 = request.RawPC("node_2")
-# Here we group our machines into lists so that we can give multiple machines the same specifications.
-nodes = [node_1, node_2]
-# Every machine we define in the "nodes" list will have the following characteristics:
-for node in nodes:
-    node.hardware_type = DEFAULT_NODE_HARDWARE_TYPE
-    node.disk_image = DEFAULT_DISK_IMAGE
-    node.addService(pg.Execute(shell="sh", command="/local/repository/init.sh"))
-    s_iface = node.addInterface(DEFAULT_LAN_SOCKET)
-    lan.addInterface(s_iface)
+request = pc.makeRequestRSpec()
+
+node1 = request.RawPC("node1")
+node2 = request.RawPC("node2")
+
+for node in [node1, node2]:
+    node.hardware_type = "c6620"
+    node.disk_image = (
+        "urn:publicid:IDN+utah.cloudlab.us+image+faasnetworkstack-PG0:baseNodeV4"
+    )
+
+# Request the 100G NICs
+iface1 = node1.addInterface("eth100g")
+iface1.component_id = "urn:publicid:IDN+utah.cloudlab.us+interface+eth100g"
+
+iface2 = node2.addInterface("eth100g")
+iface2.component_id = "urn:publicid:IDN+utah.cloudlab.us+interface+eth100g"
+
+# Explicitly connect them
+link = request.Link("p2p100g")
+link.addInterface(iface1)
+link.addInterface(iface2)
 
 pc.printRequestRSpec(request)
